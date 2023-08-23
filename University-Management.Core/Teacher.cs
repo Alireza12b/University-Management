97b8d77659f220ec79eb5220964b4ae7f60b0058
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-08-23 18:49:38 +0330

    Add project files.

---
+++ University-Management.Core/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University_Management.Core.Common;
+
+namespace University_Management.Core
+{
+    public class Teacher : BaseEntity
+    {
+    }
+}
